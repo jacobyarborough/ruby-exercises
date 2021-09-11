f2210105b921da8539afc88836f25a398ee837eb
Jacob Yarborough <jayarborough95@gmail.com>
2021-09-11 13:45:50 -0600

    Create additional tests

--- mythical-creatures/spec/wizard_spec.rb
+++ mythical-creatures/spec/wizard_spec.rb
@@ -33,20 +33,22 @@ RSpec.describe Wizard do
   end
 
   it 'starts rested' do
-    # create wizard
-    # .rested? returns true
+    wizard = Wizard.new('Gandalf')
+    expect(wizard.rested?).to eq(true)
   end
 
   it 'can cast spells' do
-    # create wizard
-    # .cast returns "MAGIC MISSILE!"
+    wizard = Wizard.new('Gandalf')
+    expect(wizard.cast).to eq("MAGIC MISSLE")
   end
 
   it 'gets tired after casting three spells' do
-    # create wizard
-    # casts spell twice
-    # check if wizard is rested
-    # casts spell
-    # check wizard is not rested
+    wizard = Wizard.new('Gandalf')
+    2.times do
+      wizard.cast
+    end
+    expect(wizard.rested?).to eq(true)
+    wizard.cast
+    expect(wizard.rested?).to eq(false)
   end
 end
