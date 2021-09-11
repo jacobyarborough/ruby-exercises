781d926924be572f9337ec6ed2118f16df45a8cd
Jacob Yarborough <jayarborough95@gmail.com>
2021-09-11 12:47:11 -0600

    Create additional tests

--- mythical-creatures/spec/pirate_spec.rb
+++ mythical-creatures/spec/pirate_spec.rb
@@ -38,14 +38,18 @@ RSpec.describe Pirate do
   end
 
   it 'has a booty' do
-    # create a pirate
-    # check that the pirate starts with 0 booty
+    pirate = Pirate.new('Anne')
+
+    expect(pirate.booty).to eq(0)
   end
 
   it 'gets 100 booty for robbing a ship' do
-    # create a pirate
-    # rob some ships
-    # check that the pirate got 100 booty for each ship it robbed
+    pirate = Pirate.new('Anne')
+    5.times do
+      pirate.rob_ship
+    end
+
+    expect(pirate.booty).to eq(500)
   end
 
 end
