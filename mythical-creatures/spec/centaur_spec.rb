2c983f811a3e81dfe9ea4e52de9ef3bdf7aeb7ba
Jacob Yarborough <jayarborough95@gmail.com>
2021-09-12 15:30:43 -0600

    Create additional tests

--- mythical-creatures/spec/centaur_spec.rb
+++ mythical-creatures/spec/centaur_spec.rb
@@ -1,4 +1,4 @@
-require './spec/spec-helper'
+require 'rspec'
 require './lib/centaur'
 
 RSpec.describe Centaur do
@@ -113,14 +113,44 @@ RSpec.describe Centaur do
   end
 
   it 'becomes rested after drinking a potion' do
-    # your code here
+    centaur = Centaur.new('George', 'Palomino')
+
+    centaur.shoot
+    centaur.run
+    centaur.shoot
+
+    expect(centaur.cranky?).to be true
+
+    centaur.drink_potion
+
+    expect(centaur.cranky?).to be false
   end
 
   it 'can only drink a potion whilst standing' do
-    # your code here
+    centaur = Centaur.new('George', 'Palomino')
+    centaur.lay_down
+
+    expect(centaur.drink_potion).to eq('NO!')
+
+    centaur.stand_up
+    centaur.shoot
+    centaur.run
+    centaur.shoot
+
+    expect(centaur.cranky?).to be true
+
+    centaur.drink_potion
+
+    expect(centaur.cranky?).to eq(false)
   end
 
   it 'gets stick if a potion is drunk while rested' do
-    # your code here
+    centaur = Centaur.new('George', 'Palomino')
+
+    expect(centaur.cranky?).to eq(false)
+
+    centaur.drink_potion
+
+    expect(centaur.sick?).to eq(true)
   end
 end
